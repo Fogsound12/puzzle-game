5b8f2f61d9a990f63c5e0f45bf971ab03c74a442
Fogsound12 <Fogsound12@users.noreply.github.com>
2026-05-19 15:31:43 +0800

    更新QR码和启动脚本指向GitHub Pages

--- start.bat
+++ start.bat
@@ -1,7 +1,3 @@
 @echo off
-cd /d "%~dp0"
-echo 正在启动拼图游戏服务器...
-start /b pythonw server.py
-timeout /t 3 >nul
-start http://127.0.0.1:8080/index.html
+start https://fogsound12.github.io/puzzle-game/
 exit
